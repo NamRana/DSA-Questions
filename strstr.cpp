fbcb83ede04a7d71eb98cb7bd3588d8f3c11d006
Naman <sanjeevnamanrana@gmail.com>
2022-11-23 09:26:06 +0530

    KMP algo

--- strstr.cpp
+++ strstr.cpp
@@ -15,3 +15,39 @@ public int strStr(String haystack, String needle){
         }
     }
 }
+
+//Using KMP
+class Solution{
+    public:
+    //calculating lps function
+    vector<int> calc(string pat,int m){
+        vector<int> lps(m,0);
+        int i=0,j=0;
+        while(i<m){
+            if(pat[i]==pat[j]){
+                j++;
+                lps[i++]=j;
+            }
+            else{
+                if(j)j=lps[j-1];
+                else lps[i++]=0;
+            }
+        }
+        return lps;
+    }
+    int strStr(string haystack, string needle){
+        int n=haystack.length(),m=needle.length();
+        if(m==0)return 0;
+        vector<int> lps=calc(needle,m);
+        int i=0,j=0;
+        while(i<n&&j<m){
+            if(haystack[i]== needle[j]){i++; j++;}
+            if(j==m)return i-j;
+            else if(i<n&&j<m&& haystack[i]!=needle[j]){
+                if(j) j=lps[j-1];
+                else i++;
+            }
+        }
+        return -1;
+    }
+}
